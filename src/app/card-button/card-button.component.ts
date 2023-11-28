39f0c209885427e678914780b28e1161a405c681
Kurcha <theyhyrik@gmail.com>
2023-11-28 04:09:03 +0300

    Main page layout changed to look more presentable

--- src/app/card-button/card-button.component.ts
+++ src/app/card-button/card-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-button',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-button.component.scss'],
 })
 export class CardButtonComponent implements OnInit {
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() link: string = '#';
+
   constructor() {}
 
   ngOnInit(): void {}
