a553c6f8c6d7b7b5748a136d249236b879f6b601
Kurcha <theyhyrik@gmail.com>
2023-11-28 03:07:28 +0300

    Basic page layout for angular app created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FileUploadComponent } from './file-upload/file-upload.component';
 import { MultipleExcelFileUploadComponent } from './multiple-excel-file-upload/multiple-excel-file-upload.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CardButtonComponent } from './card-button/card-button.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FileUploadComponent,
-    MultipleExcelFileUploadComponent,
-  ],
+  declarations: [AppComponent, MultipleExcelFileUploadComponent, HeaderComponent, FooterComponent, CardButtonComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
